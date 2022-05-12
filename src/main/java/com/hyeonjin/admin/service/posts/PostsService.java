ec49edf9b6e8e32dfcb1499a54a541514370f374
DESKTOP-E7EPOP3\hyeonjin <choihj94@gmail.com>
2022-05-12 16:59:47 +0900

    [MOD] update security config for test code

--- src/main/java/com/hyeonjin/admin/service/posts/PostsService.java
+++ src/main/java/com/hyeonjin/admin/service/posts/PostsService.java
@@ -32,7 +32,6 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
-
         return id;
     }
 
--- src/test/java/com/hyeonjin/admin/web/HelloControllerTest.java
+++ src/test/java/com/hyeonjin/admin/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.hyeonjin.admin.web;
 
+import com.hyeonjin.admin.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,7 +20,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자(SpringRunner) 실행
 // 스프링 부트 테스트와 JUnit 사이의 연결자 역할
 
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(
+        controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter( // 스캔 대상에서 SecurityConfig 제거
+                        type= FilterType.ASSIGNABLE_TYPE,
+                        classes = SecurityConfig.class
+                )
+        }
+)
 // 여러 스프링 테스트 어노테이션 중 Web(Spring MVC)에 집중할 수 있는 어노테이션
 // @Controller, @ControllerAdvice 사용 가능
 // @Service, @Component, @Repository 등은 사용할 수 없음
@@ -29,6 +41,7 @@ public class HelloControllerTest {
     // 웹 API를 테스트할 때 사용
     // 스프링 MVC 테스트의 시작지점
     // HTTP GET, POST 등에 대한 API 테스트 가능
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -43,7 +56,7 @@ public class HelloControllerTest {
                 // 응답 본문의 내용을 검증
 
     }
-
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
